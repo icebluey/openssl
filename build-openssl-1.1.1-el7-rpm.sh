10eb9c0c1331a6f50c24a4c4063f72240f2e8307
Kenneth Davidson <xylene2016@protonmail.com>
2022-01-31 04:36:24 -0500

    Update build-openssl-1.1.1-el7-rpm.sh

--- build-openssl-1.1.1-el7-rpm.sh
+++ build-openssl-1.1.1-el7-rpm.sh
@@ -56,13 +56,11 @@ sleep 1
 install -m 0755 -d ~/rpmbuild/SOURCES
 cp -pfr * ~/rpmbuild/SOURCES/
 sleep 1
-###############################################################################
+############################################################################
 
 cd /tmp
 rm -fr "${_tmp_dir}"
 rpmbuild -v -ba ~/rpmbuild/SOURCES/openssl-1.1.1.spec
-
-find ~/rpmbuild/RPMS/ -type f -iname '*.rpm'
 
 rm -fr /tmp/_output
 install -m 0755 -d /tmp/_output/openssl
